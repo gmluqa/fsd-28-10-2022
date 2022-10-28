d0316054af8c38343841fafbf06fd7ab1f9a14b3
gmluqa <gmluqa@protonmail.com>
2022-10-28 12:13:49 +0200

    seeder createdAt UpdatedAt tables set with datetimeformat, then ran npx [4mSequelize CLI [Node: 16.18.0, CLI: 6.5.1, ORM: 6.25.3][24m
    Loaded configuration file "config/config.json".
    Using environment "development".
    == 20221028092110-create-clientes-table: migrating =======
    == 20221028092110-create-clientes-table: migrated (0.007s) to seed all the seeders into the running db

--- seeders/20221028092110-create-clientes-table.js
+++ seeders/20221028092110-create-clientes-table.js
@@ -2,19 +2,20 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('clientes', [{
+      nombre: 'John Doe',
+      apellidos: "something",
+      direccion: "asdoasdasd",
+      dni: "X82721Z",
+      documentacion: "Esto es documentacion harohasrhoahrsoahsr",
+      createdAt: `${new Date().getFullYear()}-${new Date().getMonth()}-${new Date().getDay()} ${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}`,
+      updatedAt: `${new Date().getFullYear()}-${new Date().getMonth()}-${new Date().getDay()} ${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}`,
+    }], {});
+
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     /**
      * Add commands to revert seed here.
      *
