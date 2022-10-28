c5d6981254d9ef27b8a6def9769c9eb92209627c
gmluqa <gmluqa@protonmail.com>
2022-10-28 10:52:02 +0200

    added router as middleware

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const models = require('./models/index')
 
 const PORT = 3000
 
+//Middleware
 app.use(express.json())
+app.use(router)
 
 app.listen(PORT, () => {
     console.log(`Servidor conectado, puerto ${PORT}`)
